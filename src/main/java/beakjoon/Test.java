9e43754df0349cccfe98d5f4ebfbecc4fd505ce4
JTG94 <sky_engel@naver.com>
2020-07-11 15:30:49 +0900

    프로젝트 세팅

---
+++ src/main/java/beakjoon/Test.java
@@ -0,0 +1,8 @@
+package beakjoon;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hi");
+    }
+}
---
+++ src/main/java/programmers/Test.java
@@ -0,0 +1,8 @@
+package programmers;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hi");
+    }
+}
