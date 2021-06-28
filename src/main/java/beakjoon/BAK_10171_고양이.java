b9d090e9ac767c929ed9f368817c6228a6990624
JTG94 <sky_engel@naver.com>
2021-06-28 20:38:48 +0900

    210628 beakjoon - 10171 (고양이)

---
+++ src/main/java/beakjoon/BAK_10171_고양이.java
@@ -0,0 +1,14 @@
+package beakjoon;
+
+/**
+ * Date : 2021.06.28
+ * Title : 고양이 (https://www.acmicpc.net/problem/10171)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
